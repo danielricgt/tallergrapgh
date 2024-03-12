8eda18146abfafd7683ed767d86e915463aa8e20
danielricgt <drgalvan@davivienda.com>
2024-03-11 23:54:45 -0500

    quinta version

---
+++ src/schema/Mutations/Users.ts
@@ -0,0 +1,27 @@
+import {GraphQLString, graphql, graphqlSync} from 'graphql'
+import {Users} from '../../Entities/Users'
+
+export const CREATE_USER = {
+
+    type: GraphQLString,
+    args: {
+        name: {type:GraphQLString},
+        username: {type:GraphQLString},
+        password: {type:GraphQLString},
+    },
+    async resolve(_: any , args: any){
+        const {name, username, password}= args
+
+        const result = await Users.insert({
+            name: name,
+            username: username,
+            password: password
+        })
+        console.log(result)
+
+        return 'user created'
+    }
+
+
+
+}
--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,5 +1,6 @@
 // se crea una consulta para crear y definir un schema
 import {GREETING} from '../Queries/Greeting'
+import {CREATE_USER} from '../schema/Mutations/Users'
 
 
 
@@ -16,8 +17,20 @@ const RootQuery = new GraphQLObjectType({
         greeting: GREETING
     }
  })
+
+
+const Mutation = new GraphQLObjectType({
+    name: "Mutation",
+    fields: {
+        createUser: CREATE_USER
+    },
+
+})
+
+
  export const schema = new GraphQLSchema ({
-    query:RootQuery
+    query:RootQuery,
+    mutation: Mutation
 
 
 })
